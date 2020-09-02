262e21fbaf276348eeb7248776df25f6ad2826bc
sungchan <tjdcksdl00@naver.com>
2020-09-02 20:26:00 +0900

    success webrtc in local network

--- FrontEnd/TypeScript/src/api/axios.tsx
+++ FrontEnd/TypeScript/src/api/axios.tsx
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from 'axios';
 
 const axiosAPI: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/videochat', // axios 사용할 때 path앞에 baseURL이 항상 추가
-    // baseURL: 'https://ksccmp.iptime.org/videochat', // 배포
+    // baseURL: 'http://localhost:8080/videochat', // axios 사용할 때 path앞에 baseURL이 항상 추가
+    baseURL: 'https://ksccmp.iptime.org/videochat', // 배포
 });
 
 export default axiosAPI;
